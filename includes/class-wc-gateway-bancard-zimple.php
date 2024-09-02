a7c8f6db0cb841f08ef90afd0ff5c9a5e8c419be
Mario Moreno <40603707+marioungui@users.noreply.github.com>
2024-09-02 14:53:48 -0400

    Update class-wc-gateway-bancard-zimple.php

--- includes/class-wc-gateway-bancard-zimple.php
+++ includes/class-wc-gateway-bancard-zimple.php
@@ -2,7 +2,7 @@
 class WC_Gateway_Bancard_Zimple {
 
     public function process_payment($order_id) {
-        // Implement logic to process Zimple payment
+        // TODO: Implement logic to process Zimple payment
     }
 }
 ?>
